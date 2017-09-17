c028cb885d8664c40288f2d3292465af5f351442
Biechu <Nineteen99@gmail.com>
2017-09-17 11:45:31 -0700

    added view & template for detailed blog post as well as css

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    # post/pk/; the pk can only be a digit \d; any n of digits +
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
